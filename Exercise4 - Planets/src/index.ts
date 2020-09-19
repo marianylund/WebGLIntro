1e81ba3b6b1e5ad1e893db15e7b4d56decc9086b
marianylund <marianylund@pm.me>
2020-09-19 15:26:35 +0200

    Added aframe scene and code to change elements

--- Exercise4 - Planets/src/index.ts
+++ Exercise4 - Planets/src/index.ts
@@ -1,42 +1,53 @@
 import {GUI} from 'dat.gui'; 
-import {WebGLRenderer, PerspectiveCamera, Scene, BoxGeometry, MeshBasicMaterial, Mesh} from "three";
+import * as THREE from "three";
+import * as AFRAME from "aframe";
 import { AstronomicalObj } from './AstronomicalObj';
 
 
-let renderer:WebGLRenderer;
+//let renderer:WebGLRenderer;
+let scene: THREE.Scene;
 let gui: GUI;
-
+//let scene: Scene;
 let zoom = 0.02;
 
 function main() {
-  var scene = new Scene();
-  var camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  AFRAME.registerComponent('foo', main);
 
-  renderer = new WebGLRenderer();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  document.querySelector('body').appendChild(renderer.domElement);
+  scene = document.querySelector('a-scene').object3D;
+  console.log(scene.isScene);
   
-  // Only continue if WebGL is available and working
-  if (renderer === null) {
-    alert("Unable to initialize WebGL. Your browser or machine may not support it.");
-    return;
-  }
+  // var camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-  var geometry = new BoxGeometry();
-  var material = new MeshBasicMaterial( { color: 0x00ff00 } );
+  // renderer = new WebGLRenderer();
+  // renderer.setSize( window.innerWidth, window.innerHeight );
+  // document.querySelector('body').appendChild(renderer.domElement);
+  
+  // // Only continue if WebGL is available and working
+  // if (renderer === null) {
+  //   alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+  //   return;
+  // }
+
+  // var geometry = new BoxGeometry();
+  // var material = new MeshBasicMaterial( { color: 0x00ff00 } );
   var cube = new AstronomicalObj().mesh;
   scene.add(cube );
 
-  camera.position.z = 5;
+  const aSphere = document.createElement('a-sphere');
+  document.querySelector('a-scene').appendChild(aSphere);
 
-  function animate() {
-    requestAnimationFrame( animate );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render( scene, camera );
-  }
-  animate();
+  // camera.position.z = 5;
+
+  // function animate() {
+  //   requestAnimationFrame( animate );
+  //   cube.rotation.x += 0.01;
+  //   cube.rotation.y += 0.01;
+  //   renderer.render( scene, camera );
+  // }
+  // animate();
 
   };
 
+
+  
 window.onload = main;
